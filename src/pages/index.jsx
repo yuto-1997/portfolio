20f8aed946f64d0af0edfe6bd8e84df2a02449a2
shibano <ggpw326@gmail.com>
2021-02-21 22:58:19 +0900

    query表示

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../layouts'
 
-export default () => (
+export default ({ data }) => {
+  const { edges } = data.allMarkdownRemark
+  return (
   <Layout>
-    <h1>My Wonderful Website Home Page</h1>
-    <p>This is the home page</p>
+    <h1>Gatsby Tutorial Home Page</h1>
+      {edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>{node.frontmatter.title}</h3>
+          <p>{node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </div>
+    ))}
   </Layout>
-)
+)}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 100)
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+`
